7e201392c8f93ab01167f3cc5decfb8463cedeff
kenokenobingo <mail.kewe@gmail.com>
2021-06-19 21:15:25 +0200

    Only DAO members can call `trigger` functions. Added getStatus() function.

--- contracts/extensions/water/Water.sol
+++ contracts/extensions/water/Water.sol
@@ -4,24 +4,34 @@ pragma solidity ^0.8.0;
 
 import "../../core/DaoRegistry.sol";
 import "../../core/DaoConstants.sol";
+import "../../guards/AdapterGuard.sol";
+import "../../guards/MemberGuard.sol";
 
-contract WaterContract {
+contract WaterContract is
+    DaoConstants,
+    AdapterGuard,
+    MemberGuard
+{
     bool irrigation;
     uint256 timestamp;
     uint256 wateringTime;
     uint256 triggerHumidity;
     uint256 newHumidity;
 
-    function triggerWatering(uint256 humidity) public {
+    function triggerWatering(uint256 humidity, DaoRegistry dao) external onlyMember(dao) {
         irrigation = true;
         timestamp = block.timestamp;
         wateringTime = 20;
         triggerHumidity = humidity;
     }
     
-    function stopWatering(uint256 humidity) public {
+    function stopWatering(uint256 humidity, DaoRegistry dao) external onlyMember(dao) {
         irrigation = false;
         timestamp = block.timestamp;
         newHumidity = humidity;
+    }
+
+    function getStatus() public view returns(uint256) {
+        return newHumidity;
     }
 }
